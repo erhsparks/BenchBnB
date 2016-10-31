917f4c4f401162399da5ffe3be95f2cb31e95ca7
Elizabeth Sparks <erhsparks@gmail.com>
2016-10-31 10:55:16 -0700

    create session middleware to handle API login/out/signup interactions

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 
+import { login, logout, signup } from './actions/session_actions';
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(
@@ -12,4 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
   );
 
   window.store = configureStore(); //for this commit only, for testing
+  window.login = login;
+  window.logout = logout;
+  window.signup = signup;
 });
